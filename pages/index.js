ab3d12a621ccdc4bd6c66e599f4a46699d98f0e2
braincee <kwesi938@gmail.com>
2022-06-04 11:59:36 +0100

    Skiils setup

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import About from '../components/About'
 import Main from '../components/Main'
 import Navbar from '../components/Navbar'
+import Skills from '../components/Skills'
 
 
 export default function Home() {
@@ -16,6 +17,7 @@ export default function Home() {
       <Navbar />
       <Main />
       <About />
+      <Skills />
     </div>
   )
 }
