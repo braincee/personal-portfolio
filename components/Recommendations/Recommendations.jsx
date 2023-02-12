889b28ee5b6332c48b1c4ef2acc207a9943616ec
braincee <kwesi938@gmail.com>
2023-02-12 00:08:11 +0000

    refactor recommendation component

--- components/Recommendations/Recommendations.jsx
+++ components/Recommendations/Recommendations.jsx
@@ -9,28 +9,25 @@ import data from './data';
 const Recommendations = () => {
   const settings = {
     dots: true,
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        speed: 4000,
-        autoplaySpeed: 4000,
-        cssEase: "linear",
-        arrows: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 500,
+    arrows: true,
   };
 
   return (
 
     <div>
-      <div id='recommendations' className='max-w-[1240px] mx-auto px-2 py-20 mb-20'>
-        <div className="relative mb-20 grid grid-cols-2 items-center">
-      <div className="h-1 bg-[#9668f5] mr-2"></div>
-      <p className='relative inline-block text-3xl pl-2'>RECOMMENDATIONS</p>
+      <div id='recommendations' className='max-w-[1240px] mx-auto px-2 py-20 mb-20 recommendations'>
+        <div className="relative mb-20 grid grid-cols-2 items-center section">
+      <div className="h-1 bg-yellow-600 mr-2"></div>
+      <p className='relative inline-block text-3xl pl-2 text-yellow-600'>RECOMMENDATIONS</p>
     </div>
-    <div className='relative m-auto ml-10 px-10 md:max-w-[95%] md:m-auto'>
+    <div className='m-auto px-10'>
     <Slider {...settings}>
      {data.map((recommendation) => (
-       <RecommendationCard
+         <RecommendationCard
          key={recommendation.id}
          name={recommendation.name}
          title={recommendation.title}
