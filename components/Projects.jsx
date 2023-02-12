1c5068a09c0455998173c65037843c862749f71c
braincee <kwesi938@gmail.com>
2023-02-12 00:06:47 +0000

    refactor project component

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -7,22 +7,24 @@ import ProjectItem from './ProjectItem';
 
 const Projects = () => {
   return (
-    <div id='projects' className='w-full mb-20'>
+    <div id='projects' className='w-full mb-20 projects'>
       <div className='max-w-[1240px] mx-auto px-2 py-16'>
-      <div className="relative mb-20 grid grid-cols-2 items-center">
-          <div className="h-1 bg-[#9668f5] mr-2"></div>
-          <p className='relative inline-block text-xl pl-2 md:text-3xl'>RECENT WORKS</p>
+      <div className="relative mb-20 grid grid-cols-2 items-center section">
+          <div className="h-1 bg-yellow-600 mr-2"></div>
+          <p className='relative inline-block text-3xl pl-2 text-yellow-600'>RECENT WORKS</p>
         </div>
         <div className='grid md:grid-cols-3 gap-8 max-w-[90%] mx-auto'>
           <ProjectItem
             title='Property Finder'
             backgroundImg={propertyImg}
             projectUrl='/property'
+            Technology="React | Redux"
           />
           <ProjectItem
             title='Calculator App'
             backgroundImg={calculatorImg}
             projectUrl='/calculator'
+            Technology="React | JavaScript"
           />
           <ProjectItem
             title='Netflix App'
