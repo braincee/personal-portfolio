aac89468b4f718413bffba7ea70eb9834ae18619
braincee <kwesi938@gmail.com>
2023-02-12 00:07:43 +0000

    refactor recommendation card

--- components/Recommendations/RecommendationCard.jsx
+++ components/Recommendations/RecommendationCard.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 const RecommendationCard = ({ name, title, image, text, link }) => {
     return (
-      <div className="md:grid grid-cols-4 bg-[#1166b128] py-5 rounded-2xl">
+      <div className='card-1'>
+        <div className="md:grid grid-cols-4 bg-[#1166b128] py-5 rounded-2xl card">
         <div className="col-span-1 flex flex-col items-center ml-2">
           <img src={image} alt={name} className="rounded-full w-40 h-40" />
           <p className="text-xl font-semibold mt-2">{name}</p>
@@ -12,11 +13,12 @@ const RecommendationCard = ({ name, title, image, text, link }) => {
           <p>{text}</p>
           <div className="mt-5 mr-10 flex justify-end">
             <a href={link} className="fa-linkedin text-[#e0d7d7] text-xl" target="_blank" rel="noopener noreferrer justify-end">
-           <button className='rounded-md bg-[#1167b1] h-10 w-20'>LinkedIn</button>
+           <button className='rounded-full bg-[#1167b1] px-10 py-3'>LinkedIn</button>
          </a>
       </div>
         </div>
-    </div>
+      </div>
+      </div>
     );
   };
   
