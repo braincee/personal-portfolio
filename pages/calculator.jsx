6d4e85bf5d754151b1da881a4914fb38922fee58
braincee <kwesi938@gmail.com>
2023-02-19 02:06:29 +0000

    refactor the project pages

--- pages/calculator.jsx
+++ pages/calculator.jsx
@@ -9,45 +9,39 @@ import Link from 'next/link';
 const calculator = () => {
   return (
     <div className='project'>
-      <div className='lg:h-[40vh] project-image'>
+      <div className='project-image'>
         <div className='relative top-0 left-0 w-full bg-black/80 z-10 project-image-1' />
         <Image
           className='absolute z-1'
           layout='fill'
           objectFit='cover'
           src={calculatorImg}
-          alt='/'
+          alt='/calculator-image'
         />
         <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
           <h2 className='py-2 text-yellow-600'>Calculator App</h2>
-          <p>React JS</p>
+          <p>JavaScript | React JS</p>
         </div>
       </div>
 
-      <div className='mx-auto p-2 pt-8'>
-        <div>
-          <p className='text-yellow-600 text-4xl py-5'>Project Outline</p>
-          <p className='italic text-xl m-2'>
-            This app was built using React JS and is hosted on Firebase. Users
-            are able to search properties based on an Address, City, or ZIP code
-            to retrieve a list of active properties currently for sale. You will
-            be able to view property information as well as the specific
-            location of the property integrated with the Google Maps API. User
-            authentication is available so you can signup & signin to your
-            account with an email address in order to save your favorite
-            properties. This is made possible with Zillow API.
+      <div className='mx-auto p-2 pt-20 lg:grid grid-cols-12 lg:pt-2 project-outline'>
+        <div className='col-span-10 lg:m-[20px]'>
+          <p className='text-yellow-600 text-[70px] py-10 tracking-widest lg:text-[40px]'>Project Outline</p>
+          <p className='italic text-[60px] m-4 lg:text-[25px]'>
+          "Math Magicians" is a website for all fans of mathematics. 
+          It is a Single Page App (SPA) that allows users to make a simple calculations 
+          and read a random math-related quote. Built with React, Sass and JavaScript.
           </p>
-        </div>
-        <div className='flex justify-around m-2 py-10'>
+        <div className='flex justify-around m-2 py-20'>
             <div className='relative'>
-              <Link href='/'>
+              <Link href='https://math-magicians-braincee.netlify.app/'>
                 <p className='rounded-full py-2 px-[30px] bg-yellow-600 flex items-center'>
                   <RiLiveFill size={20} className='pr-1' /> Live 
                 </p>
                 </Link>
               </div>
             <div className='relative'>
-              <Link href='/'>
+              <Link href='https://github.com/braincee/math-magicians'>
                 <p className='rounded-full py-2 px-[30px] bg-yellow-600 flex items-center'>
                   <FiGithub size={20} className='pr-1' /> GitHub 
                 </p>
@@ -61,10 +55,11 @@ const calculator = () => {
             </Link>
             </div>
           </div>
-        <div className='col-span-4 md:col-span-1 shadow-sm shadow-yellow-400 rounded-2xl p-4 mt-20'>
+        </div>
+        <div className='col-span-2 shadow-sm shadow-yellow-400 rounded-2xl p-4 mt-20 lg:h-[50vh] lg:w-[15vw]'>
           <div className='p-2'>
             <p className='text-center font-bold pb-2'>Technologies</p>
-            <div className='flex items-center justify-around'>
+            <div className='flex items-center justify-around lg:flex-col'>
               <p className='text-yellow-600 py-2 flex items-center'>
                 <RiRadioButtonFill className='pr-1' /> JavaScript
               </p>
